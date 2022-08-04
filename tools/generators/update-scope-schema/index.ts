b305cba8f1ca2ce0f03ef1c25825cd23776fbe98
poonam <poonam.mishra@elekta.com>
2022-08-04 16:38:31 +0100

    lab 14a changes

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,9 +1,31 @@
-import { Tree, formatFiles, updateJson } from '@nrwl/devkit';
+import { ProjectConfiguration, getProjects, Tree, formatFiles, updateJson, names } from '@nrwl/devkit';
 
-export default async function (host: Tree) {
-  await updateJson(host, 'nx.json', (nxJson) => {
-    nxJson.defaultProject = 'api';
-    return nxJson;
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags
+        // take only those that point to scope
+        .filter((tag: string) => tag.startsWith('scope:'))
+    )
+    // flatten the array
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    // remove prefix `scope:`
+    .map((scope: string) => scope.slice(6));
+  // remove duplicates
+  return Array.from(new Set(allScopes));
+}
+
+export default async function (tree: Tree) {
+  const scopes = getScopes(getProjects(tree));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', (schemaJson) => {
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: names(scope).className,
+    }));
+    schemaJson.properties.directory.enum = scopes
+ 
+    return schemaJson;
   });
-  await formatFiles(host);
+  await formatFiles(tree);
 }
