f954a51d03740affe93b2b6dc127259019de8896
poonam <poonam.mishra@elekta.com>
2022-08-04 14:08:14 +0100

    lab 13b changes

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,17 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
+export interface UtilLibSchema {
+  name : string; 
+  directory : 'store' | 'api' | 'shared'
+}
+
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  await libraryGenerator(tree, { 
+    name: `util-${schema.name }`,
+    directory : schema.directory,
+    tags: `type: util, scope : ${schema.directory}`
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
